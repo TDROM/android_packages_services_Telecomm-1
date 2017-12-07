a19274581cb9f428590b0613eec565f18d53da4c
sdhz150 <yanghui140@gmail.com>
2017-12-07 17:47:03 +0800

    归属地

--- Android.mk
+++ Android.mk
@@ -6,6 +6,8 @@ include $(CLEAR_VARS)
 LOCAL_JAVA_LIBRARIES := telephony-common telephony-ext
 LOCAL_STATIC_JAVA_LIBRARIES := ims-ext-common
 
+LOCAL_STATIC_JAVA_LIBRARIES += libSudaSdk
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-proto-files-under, proto)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
